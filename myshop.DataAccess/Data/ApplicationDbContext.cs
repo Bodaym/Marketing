ef76bb4a22ccc21b96c5d32e3f981ab702fe8b6a
Abdullah Abo El-naga <abdullah.elbakry57@gmail.com>
2025-03-09 04:45:08 +0200

    Add files via upload

---
+++ myshop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using myshop.Entities.Models;
+
+namespace myshop.DataAccess
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // تحديد الدقة للخصائص decimal لتجنب فقدان البيانات
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderDetail>()
+                .Property(od => od.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderHeader>()
+                .Property(oh => oh.TotalPrice)
+                .HasPrecision(18, 2);
+        }
+    }
+}
