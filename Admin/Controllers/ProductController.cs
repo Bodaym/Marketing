9eede358970cd97ee39c887193fc07340928cae9
Abdullah Abo El-naga <abdullah.elbakry57@gmail.com>
2025-03-09 04:47:34 +0200

    Add files via upload

---
+++ Admin/Controllers/ProductController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using myshop.Entities.Models;
+using myshop.Entities.Repositories;
+using myshop.Entities.ViewModels;
+
+namespace myshop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var products = _unitOfWork.Product.GetAll(Includeword: "Category");
+            return Json(new { data = products });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductVM productVM, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                string rootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString();
+                    var uploadPath = Path.Combine(rootPath, "Images", "Products");
+                    var ext = Path.GetExtension(file.FileName);
+
+                    using (var filestream = new FileStream(Path.Combine(uploadPath, filename + ext), FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+                    productVM.Product.Img = Path.Combine("Images", "Products", filename + ext);
+                }
+
+                _unitOfWork.Product.Add(productVM.Product);
+                _unitOfWork.Complete();
+                TempData["Create"] = "Item has been created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(productVM);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ProductVM productVM = new ProductVM()
+            {
+                Product = _unitOfWork.Product.GetFirstorDefault(x => x.Id == id),
+                CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProductVM productVM, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string rootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString();
+                    var uploadPath = Path.Combine(rootPath, "Images", "Products");
+                    var ext = Path.GetExtension(file.FileName);
+
+                    if (!string.IsNullOrEmpty(productVM.Product.Img))
+                    {
+                        var oldImgPath = Path.Combine(rootPath, productVM.Product.Img.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImgPath))
+                        {
+                            System.IO.File.Delete(oldImgPath);
+                        }
+                    }
+
+                    using (var filestream = new FileStream(Path.Combine(uploadPath, filename + ext), FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+                    productVM.Product.Img = Path.Combine("Images", "Products", filename + ext);
+                }
+
+                _unitOfWork.Product.Update(productVM.Product);
+                _unitOfWork.Complete();
+                TempData["Update"] = "Data has been updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(productVM);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productInDb = _unitOfWork.Product.GetFirstorDefault(x => x.Id == id);
+            if (productInDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            if (!string.IsNullOrEmpty(productInDb.Img))
+            {
+                var imgPath = Path.Combine(_webHostEnvironment.WebRootPath, productInDb.Img.TrimStart('\\'));
+                if (System.IO.File.Exists(imgPath))
+                {
+                    System.IO.File.Delete(imgPath);
+                }
+            }
+
+            _unitOfWork.Product.Remove(productInDb);
+            _unitOfWork.Complete();
+            return Json(new { success = true, message = "Product has been deleted successfully" });
+        }
+    }
+}
